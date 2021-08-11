23d6ec9d090dcf5b242de8435d9e541fb31cba8d
BAtabaiev <87579276+BAtabaiev@users.noreply.github.com>
2021-08-11 17:37:12 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,103 @@
+/*let x1,x2, x3, x4;
+x1='hello, js';
+x2=100;
+x3=5.5;
+x4=true;
+//const COLOR_COLOR = RED;
+console.log(x1, x2, x3, x4);
+alert(`Results: ${x1} ${x2} ${x3} ${x4}`);
+
+let a, h, s;
+//a= 10, h= 23;
+a=prompt("Enter value a");
+h=prompt("Enter value h");
+s=1/2*a*h;
+console.log('Площадь треугольника' , s);
+alert(`Площадь треугольника ${s}`);
+
+let x1=100, x2=200;
+res=x1+x2;
+console.log(res);
+let x3='Hello', x4='Bohdan';
+res=x3+ ', '+x4+'!';
+console.log(res);
+
+let x1 = +prompt('1 number');
+let x2 = +prompt('2 numer');
+res = x1+x2;
+alert(res);  
+
+let x1=10, x2=3;
+console.log('ostatok ot deleniya', x1 % x2);
+console.log('vozvedenie v stepen', x1 ** x2); 
+
+let userValue = 537;
+
+let firstDigit = userValue % 7;
+console.log(firstDigit);
+let secondDigit = userValue % 6;
+console.log(secondDigit);
+let lastDigit = userValue % 10;
+console.log(lastDigit);
+
+res=25 ** (1/2);
+console.log(`Корень квадратный из 25 - ${res}!`);
+res=1000 ** (1/3);
+console.log(`Корень кубический из 1000 - ${res}!`)
+
+let x1=5;
+res=-x1;
+console.log(res);
+
+let x1=10;
+console.log(x1 % 2 == 0);
+
+
+let userAge;
+userAge = prompt('Enter some value');
+
+if(userAge >=18){
+	alert('Вы совершеннолетний!!!');
+} else {
+    userAge <18
+	alert('Заходите через годик-второй!');
+}
+
+
+let userNumber;
+userNumber = prompt ('Введите число');
+
+
+let userCalc;
+userCalc = prompt ('Сколько будет 2+2?');
+if (userCalc==4) {
+	alert('Правильно!');
+}
+if(userCalc !=4) {
+	alert('Неправильно');
+}
+*/
+let exBook, price1=10, penCil, price2=20, totalBuy;
+exBook = prompt ('Сколько тетрадей вам нужно?');
+penCil = prompt ('Сколько ручек вам нужно?');
+totalBuy= exBook*price1 + penCil*price2;
+if (totalBuy >= 500){
+	totalBuy*=0.98;
+}
+alert(`Всего с вас ${totalBuy} грн!`);
+
+
+/*
+let value1 = prompt('Введите любое число');
+if(value1< 10){
+	value1*=30;
+} else{
+	value1-=100;
+}
+alert(value1);
+*/
+
+
+
+
+
